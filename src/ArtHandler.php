64761c0d9b09aa957c8b713293ac8c3c0b3f8665
axharus <axharus@gmail.com>
2020-06-12 16:14:47 +0300

    inner error fix

--- src/ArtHandler.php
+++ src/ArtHandler.php
@@ -5,26 +5,40 @@ namespace Axharus\ArtHandler;
 
 
 use App\Exceptions\Handler;
+use Illuminate\Auth\AuthenticationException;
 use \Illuminate\Contracts\Debug\ExceptionHandler;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Throwable;
 
 class ArtHandler extends Handler implements ExceptionHandler {
 
     public function report($e) {
-
-        try {
-            $message = "Status code => ".$e->getStatusCode()."\n";
-            $message .= "Info => ".$e->getMessage()."\n";
-            $message .= "Trace => ".$e->getTraceAsString();
+        if(!strpos($e->getFile(), 'axharus/art-handler')){
+            $status = 0;
+            $message = "";
+            if($e instanceof AuthenticationException){
+                $message = "Status code => 403\n";
+                $message .= "Info => ".$e->getMessage()."\n";
+                $message .= "Trace => ".$e->getTraceAsString();
+                $status = 403;
+            }elseif($e instanceof HttpException){
+                $message = "Status code => ".$e->getStatusCode()."\n";
+                $message .= "Info => ".$e->getMessage()."\n";
+                $message .= "Trace => ".$e->getTraceAsString();
+                $status = $e->getStatusCode();
+            }else{
+                $message = "Status code => ".$e->getCode()."\n";
+                $message .= "Info => ".$e->getMessage()."\n";
+                $message .= "Trace => ".$e->getTraceAsString();
+                $status = $e->getCode();
+            }
             $sender  = new ArtHandlerSender([
                 "type"          => "php",
                 "error_content" => $message,
                 "screen_size"   => "",
-                "status_code"      => $e->getStatusCode()
+                "status_code"      => $status
             ]);
             $sender->send();
-        } catch (\Exception $e) {
-            var_dump('ArtHandlerFail');
         }
 
         return parent::report($e);
